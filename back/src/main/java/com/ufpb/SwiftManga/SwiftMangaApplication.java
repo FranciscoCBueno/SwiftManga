dcd918d1eae1bfa3bf7f62907f70911243008c54
Francisco Bueno <francostab@hotmail.com>
2024-09-12 23:02:51 -0300

    Criação do projeto

---
+++ back/src/main/java/com/ufpb/SwiftManga/SwiftMangaApplication.java
@@ -0,0 +1,13 @@
+package com.ufpb.SwiftManga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftMangaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftMangaApplication.class, args);
+	}
+
+}
---
+++ front/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import './components/Login/Login'
+import './components/Login/Login';
+import { Login } from './components/Login/Login';
+
+function App() {
+  return (
+    <Login></Login>
+  );
+}
+
+export default App;
